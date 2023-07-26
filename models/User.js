ce882f564e9b98bdec7323e265b8c9da6e161af7
Steven Tovar-Contreras <123904059+s-tovar@users.noreply.github.com>
2023-07-25 23:04:15 -0700

    installed dependencies and added function to the models folder, User.js file

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true, 
+        unique: true,
+        match: [/.+\@.+\..+/, 'Must have valid email address']
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+}, { toJSON: { vistuals: true } });
+
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+module.exports = model('User', UserSchema);
