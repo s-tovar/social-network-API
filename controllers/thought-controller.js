73ae4c26e9dcb1f0e7e522ccd91973b1056ff2d2
Steven Tovar-Contreras <123904059+s-tovar@users.noreply.github.com>
2023-08-03 13:31:51 -0700

    added function to update a thought, and add and remove a reaction to thought-controller.js file

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -36,11 +36,46 @@ const thoughtController = {
         })
         .catch(err => res.json(err));
     },
+    updateThought({ params, body }, res) { //update a thought
+        Thought.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'Sorry! No user found with this id' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.jsaon(err));
+    },
     deleteThought({ params }, res) { //delete a thought
         Thought.findOneAndDelete({ _id: params.id })
         .then(dbThoughtData => res.json(dbThoughtData))
         .catch(err => res.json(err));
     },
+    addReaction({ params, body }, res) {
+        Thought.findOneAndUpdate( 
+            { _id: params.thoughtId },
+            { $push: { reactions: body } },
+            { new: true, runValidators: true }
+        )
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'Sorry! No user found with this id' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.json(err));
+    },
+    removeReaction({ params, body }, res) {
+        Thought.findOneAndUpdate( 
+            { _id: params.thoughtId },
+            { $pull: { reactions: { reactionId: params.reactionId } } },
+            { new: true }
+        )
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => res,json(err));
+    }
 };
 
 module.exports = thoughtController;
