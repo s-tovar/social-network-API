4807887cd9cba6ddb4772c306c4d611c85abf789
Steven Tovar-Contreras <123904059+s-tovar@users.noreply.github.com>
2023-08-03 09:00:31 -0700

    continued thoughtController by adding functions to get thought by the id, creating and deleting a thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -9,4 +9,38 @@ const thoughtController = {
             res.sendStatus(400);
         });
     },
-}
+    getThoughtById({ params }, res) { // get thought by the ID
+        Thought.findOne({ _id: params.id })
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    },
+    //create a thought
+    createThought({ body }, res) {
+        Thought.create(body)
+        .then(({ _id }) => {
+            return User.findOneAndUpdate(
+                { _id: body.userId },
+                { $push: { thoughts: _id } },
+                { new: true }
+            );
+        })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'Sorry! No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err));
+    },
+    deleteThought({ params }, res) { //delete a thought
+        Thought.findOneAndDelete({ _id: params.id })
+        .then(dbThoughtData => res.json(dbThoughtData))
+        .catch(err => res.json(err));
+    },
+};
+
+module.exports = thoughtController;
