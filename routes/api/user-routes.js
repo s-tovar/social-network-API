0625145d881a35d72a6c8e609635f6c38ab7bf63
Steven Tovar-Contreras <123904059+s-tovar@users.noreply.github.com>
2023-08-06 19:49:42 -0700

    finished user-routes.js file

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -11,7 +11,19 @@ const {
 } = require('../../controllers/user-controller');
 
 // GET all and POST at api/users
-router.route("/").get(getAllUser).post(createUser);
+router.route("/")
+.get(getAllUser)
+.post(createUser);
 
 // GET one, PUT, & DELETE at api/users/id
-router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+router.route("/:id")
+.get(getUserById)
+.put(updateUser)
+.delete(deleteUser);
+
+//POST and DELETE for api/users/:userId/friends/friendId
+router.route('/:/userId/friends/:friendId')
+.post(addFriend)
+.delete(removeFriend);
+
+module.exports = router;
