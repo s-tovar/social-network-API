3ebcd09ccff200551ee7d9ce858fbf756ed83ef2
Steven Tovar-Contreras <123904059+s-tovar@users.noreply.github.com>
2023-08-06 19:56:06 -0700

    created similar function from user-routes into thought-routes.js

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+
+const { 
+    getAllThought,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thought-controller');
+
+// GET all and POST at api/thoughts
+router.route("/")
+.get(getAllThought)
+.post(createThought);
+
+// GET one, PUT, & DELETE at api/thought/id
+router.route("/:id")
+.get(getThoughtById)
+.put(updateThought)
+.delete(deleteThought);
+
+//POST and DELETE for api/thoughts/:thoughtId/reactions
+router.route('/:/thoughtId/reactions')
+.post(addReaction)
+.delete(removeReaction);
+
+module.exports = router;
