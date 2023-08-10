ee9abe7e8df97c749e5f2fbd2d055e24382bb80d
Steven Tovar-Contreras <123904059+s-tovar@users.noreply.github.com>
2023-08-10 11:43:07 -0700

    debugged routes in api and server.js file

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const mongoose = require('./config/connection');
 const routes = require('./routes');
 
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
