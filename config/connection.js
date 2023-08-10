abfcca0073c3e4c6f4f66f483e35251bf5ce1a36
Steven Tovar-Contreras <123904059+s-tovar@users.noreply.github.com>
2023-08-10 08:51:17 -0700

    debugged connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social_network_db', {
-    userFindAndModify: false,
-    userNewUrlParser: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
