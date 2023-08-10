2bab72f604b14c9377ce490c15767fa3872e4c9b
Steven Tovar-Contreras <123904059+s-tovar@users.noreply.github.com>
2023-08-09 19:04:01 -0700

    fixed server.js file now that routes file has been added

--- server.js
+++ server.js
@@ -1,27 +1,16 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const userRoutes = require('./routes/api/user-routes');
-const thoughtRoutes = require = ('./routes/api/though-routes');
+const mongoose = require('./config/connection');
+const routes = require('./routes');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(routes);
 
-// use api routes
-
-app.use('/api/users', userRoutes);
-app.use('/api/thoughts', thoughtRoutes);
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-media-api', {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-// use to log mongo queries that are being execued
-mongoose.set('debug', true);
-
-app.listen(PORT, () => 
-console.log(`connected on localhost:${PORT}`));
+mongoose.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+    });
+  });
