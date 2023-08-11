17ea04e8debf1f21c2d5c85025f9362bf445ec89
Steven Tovar-Contreras <123904059+s-tovar@users.noreply.github.com>
2023-08-10 19:08:02 -0700

    debugged index.js routes to ./api folder and corrected connection.js file in config folder

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,16 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social_network_db', {
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social_network_db', {
+//     useFindAndModify: false,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// });
 
-mongoose.set('debug', true);
-module.exports = mongoose.connection;
+// mongoose.set('debug', true);
+// module.exports = mongoose.connection;
+
+const { connect, connection } = require("mongoose");
+
+connect("mongodb://127.0.01:27017/socialMediaApi");
+
+module.exports = connection;
